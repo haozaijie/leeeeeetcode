93cff40872743b144178c207fb970b5e493f496e
haozaijie <haozaijie@gmail.com>
2020-12-11 05:22:16 -0500

    add optimal method for two sum

--- algo/1_Two_Sum.py
+++ algo/1_Two_Sum.py
@@ -1,6 +1,17 @@
+# method 1 - brute force, the complexity is O(n^2)
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)-1):
             for j in range(i+1, len(nums)):
                 if nums[i]+nums[j]==target:
                     return [i,j]
+
+
+# method 2 - create a value to index dictionary, return the result if the desired value is already there, otherwise add the value to index pair to the dict; the time complexity is O(n)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        table = {}
+        for i, num in enumerate(nums):
+            if target - num in table:
+                return [table[target-num], i]
+            table[num] = i 
